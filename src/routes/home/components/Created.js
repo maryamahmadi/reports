e3a8685dce52eb7e77477fb5930b5fe3385a3641
Maryam Ahmadi <maryam.ahmadi@nokia.com>
2020-08-19 17:11:25 -0400

    acknowledging when a report is successfully created

---
+++ src/routes/home/components/Created.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Created () {
+    return (<div style={{paddingTop: '30vh', paddingLeft: '30vw', color: '#124191', fontSize: '28px'}}>
+        Thank you for submitting your report!
+    </div>)
+}
+
+export default Created
--- src/routes/home/components/NewReportFormParent.js
+++ src/routes/home/components/NewReportFormParent.js
@@ -1,21 +1,68 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
+import Created from './Created'
 
 function NewReportFormParent () {
-    const onSubmit = (event) => {
-        // submit
-        console.log('name', name)
-        console.log('last', lastWeek)
-        console.log('next', nextWeek)
-        console.log('comments', comments)
+    const CustomButton = withStyles({
+        root: {
+            width: '100px',
+            color: 'white',
+            backgroundColor: '#124191',
+            '&:hover': {
+                backgroundColor: '#1a59c4'
+            },
+        }
+    })(Button)
+    const [created, setCreated] = useState(false)
+    useEffect(() => setCreated(false), [])
+    const onSubmit = async (event) => {
+        event.persist()
         event.preventDefault()
-    }
-    const [name, setName] = useState('')
-    const [lastWeek, setLastWeek] = useState('')
+        try {
+            const resp = await fetch('/api/reports', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username, 
+                    thisWeek, 
+                    nextWeek,
+                    comments: comments
+                })
+            })
+            const result = await resp.json()
+            if (!resp.ok) {
+                // setError(
+                //   'description',
+                //   'error',
+                //   result.message || 'Internal Server Error'
+                // )
+                return
+            } else {
+                setCreated(true)
+            }
+        } catch (e) {
+            if (e?.code === 'MISSING_TITLE' || e?.code === 'INVALID_TITLE') {
+                // setError('title', 'error', e.message)
+            } else {
+                // setError('description', 'error', 'Internal Server Error')
+            }
+            console.error(e)
+        }
+      }
+
+    const [username, setUsername] = useState('')
+    const [thisWeek, setThisWeek] = useState('')
     const [nextWeek, setNextWeek] = useState('')
     const [comments, setComments] = useState('')
-    return (
+    if (created) {
+        return <Created />
+    } else {
+        return (
         <div style={{padding: '20px'}}>
+            <div style={{fontSize: '28px', fontWeight: '700', paddingBottom: '10px', color: '#124191'}}> Submit Your Report </div>
             <form onSubmit={onSubmit}>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
                     <label style={{paddingBottom: '5px'}}>
@@ -23,8 +70,8 @@ function NewReportFormParent () {
                     </label>
                     <select 
                         style={{width: '300px', marginBottom: '20px', padding: '4px 0 4px 0'}}
-                        value={name} 
-                        onChange={(event)=> setName(event.target.value)}>
+                        value={username} 
+                        onChange={(event)=> setUsername(event.target.value)}>
                         <option value='' disabled defaultValue hidden>Select Your Name</option>
                         <option value='ahmad'>Ahmad</option>
                         <option value='alanI'>Alan I</option>
@@ -45,17 +92,17 @@ function NewReportFormParent () {
                     </select>   
                     <label 
                     style={{paddingBottom: '5px'}}>
-                        What did you do last week?
+                        What did you do this week?
                     </label>
                     <textarea 
                         style={{width: '700px', height: '150px', marginBottom: '20px'}}
-                        value={lastWeek} 
-                        onChange={(event)=> setLastWeek(event.target.value)} />
+                        value={thisWeek} 
+                        onChange={(event)=> setThisWeek(event.target.value)} />
                     <label style={{paddingBottom: '5px'}}>
                         What will you be working on next week?
                     </label>
                     <textarea 
-                        style={{width: '700px', height: '150px', marginBottom: '20px'}}
+                        style={{width: '700px', height: '100px', marginBottom: '20px'}}
                         value={nextWeek} 
                         onChange={(event)=> setNextWeek(event.target.value)} />
                     <label style={{paddingBottom: '5px'}}>
@@ -65,12 +112,10 @@ function NewReportFormParent () {
                         style={{width: '700px', height: '50px', marginBottom: '20px'}}
                         value={comments} 
                         onChange={(event)=> setComments(event.target.value)} />
-                    <Button type='submit'>Submit</Button>
+                    <CustomButton type='submit'>Submit</CustomButton>
                     </div>
             </form>
-        </div>
-    )
+        </div>)
+    }
 }
 export default NewReportFormParent
-// <textArea value={this.state.value} onChange={this.handleChange}
-// <select value={this.state.value} onChange={this.handleChange}
