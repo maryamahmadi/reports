16e42ff3c4034761026b23428fe3e385785a0d9b
Maryam <maryam.ahmadi@gmail.com>
2020-09-29 21:23:59 -0400

    making Home pictures circle

--- src/routes/home/components/index.tsx
+++ src/routes/home/components/index.tsx
@@ -15,13 +15,13 @@ function Home() {
     >
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <Link style={{ color: 'white', textDecoration: 'none' }} to="/create-report">
-          <img alt="lazy employee" src={employees} width="200px" height="200px" />
+          <img alt="lazy employee" src={employees} style={{ width: '200px', height: '200px', borderRadius: '50%' }} />
         </Link>
         <div style={{ color: '#124191', fontSize: '28px', fontWeight: 'bold' }}>Employee </div>
       </div>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <Link style={{ color: 'white', textDecoration: 'none' }} to="/boss">
-          <img alt="boss" src={SteveMorse} width="200px" height="200px" />
+          <img alt="boss" src={SteveMorse} style={{ width: '200px', height: '200px', borderRadius: '50%' }} />
         </Link>
         <div style={{ color: '#124191', fontSize: '28px', fontWeight: 'bold' }}>Boss </div>
       </div>
